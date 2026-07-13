25e951f3e8cfe3187d742707a96cc9fa03d88614
snur47981-hash <snur47981@gmail.com>
2026-07-13 18:14:50 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "🚀 Starting Kali Linux on Railway..."
-echo "⚡ Memory: $(free -h | grep Mem | awk '{print $2}')"
-echo "💾 CPU: $(nproc) cores"
-echo "🌐 IP: $(hostname -I | awk '{print $1}')"
-echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-exec /bin/ttyd -p 7681 -c ${USERNAME}:${PASSWORD} /bin/bash
